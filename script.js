9e7eb4d770610091d80ca79e47613b81e2028a8c
shahin <90312633+shahin-salim@users.noreply.github.com>
2021-11-05 13:40:17 +0530

    Add files via upload
    nav bar added

---
+++ script.js
@@ -0,0 +1,12 @@
+const inputBox = document.getElementById("div-top-of-inuput-box");
+const hideBtn = document.getElementById("x-icon-on-input");
+const inputbox = document.getElementById("input-box");
+
+inputBox.addEventListener("click", function () {
+     inputBox.style.visibility = "hidden";
+     inputbox.focus();
+});
+
+hideBtn.addEventListener("click", function () {
+     inputBox.style.visibility = "visible";
+});
