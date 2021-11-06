1ccefdfe79f7720d3cb7e917c7e63578462e8065
shahin <90312633+shahin-salim@users.noreply.github.com>
2021-11-07 02:56:56 +0530

    Add files via upload
    vedio play on click

--- script.js
+++ script.js
@@ -10,16 +10,82 @@ inputBox.addEventListener("click", function () {
 hideBtn.addEventListener("click", function () {
      inputBox.style.visibility = "visible";
 });
-const play = document.querySelector("video");
-let pAndP = 0 ;
-play.addEventListener("click",function(){
-     if(pAndP==0){
-          play.play();
-          pAndP++;
-     }else{
-          play.pause();
-          pAndP--;
+const vOne = document.getElementById("videoOne");
+const vTwo = document.getElementById("videoTwo");
+const vThree = document.getElementById("video-3");
+const vFour = document.getElementById("videoFour");
+
+let onOff = 0;
+let onOff_two = 0;
+let three = 0;
+let four = 0 ;
+
+vOne.addEventListener("click", function () {
+     onOff_two = 0;
+     three = 0;
+     vTwo.pause();
+     vThree.pause();
+     four = 0 ;
+     vFour.pause();
+     if (onOff == 0) {
+          vOne.play();
+          onOff = 1;
+     } else {
+          onOff = 0;
+          vOne.pause();
      }
-})
+     console.log("fisry");
+});
+
+vTwo.addEventListener("click", function () {
+     onOff = 0;
+     vOne.pause();
+     vThree.pause();
+     three = 0;
+     four = 0;
+     vFour.pause();
+     if (onOff_two == 0) {
+          vTwo.play();
+          onOff_two = 1;
+     } else {
+          onOff_two = 0;
+          vTwo.pause();
+     }
+     console.log("second");
+});
+
+vThree.addEventListener("click", function () {
+     onOff = 0;
+     onOff_two = 0;
+     four = 0 ;
+     vOne.pause();
+     vTwo.pause();
+     vFour.pause();
+     if (three == 0) {
+          vThree.play();
+          three = 1;
+     } else {
+          three = 0;
+          vThree.pause();
+     }
+     console.log("fourth");
+});
+
+vFour.addEventListener("click", function () {
+     onOff = 0;
+     onOff_two = 0;
+     three = 0 ;
+     vOne.pause();
+     vTwo.pause();
+     vThree.pause();
+     if (four == 0) {
+          vFour.play();
+          four = 1;
+     } else {
+          four = 0;
+          vFour.pause();
+     }
+     console.log("fourth");
+});
 
 
